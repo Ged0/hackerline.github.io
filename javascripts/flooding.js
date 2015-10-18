a3d140ad1e1a6d1631fca5fde178e22dedd54e55
zhaohui <linezhao@sohu-inc.com>
2015-10-18 19:05:51 +0800

    add js

---
+++ javascripts/flooding.js
@@ -0,0 +1,32 @@
+/*
+* by line
+* email£ºzh.hackerline@gmail.com
+*/
+
+function auto_sendmsg(){
+	document.getElementById('chart_content').value = document.getElementById('area').value;
+	sendmsg();
+	setTimeout(auto_sendmsg(),send_interval_time*1000+100);
+	
+}
+
+function set(){
+	var str = document.getElementById('area').value;
+	console.log(str);
+}
+	
+function set1(){
+	var str = document.getElementById('area').value;
+	document.getElementById('area1').value = document.getElementById('area1').value + str;
+	console.log(document.getElementById('area1').value)
+}
+	
+function set_time(){
+	set1();
+	setTimeout('set_time()',1000);
+}
+	
+function test(){
+	//	document.getElementById('area1').value = "test";
+	console.log("test");
+}
